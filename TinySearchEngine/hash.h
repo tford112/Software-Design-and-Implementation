d9e637660ff315534db4ef81b51cacc3fe7a10bd
Tom Varghese <tomtomnv@gmail.com>
2020-05-24 03:04:46 -0400

    Add files via upload
    adding the rest of the provided files containing the functions from the class

---
+++ TinySearchEngine/hash.h
@@ -0,0 +1,15 @@
+//! This is some utilites for CS23 search engine project ( or general case)
+//! \author wei pan (wei.pan@dartmouth.edu)
+//! \brief: Three hash funtions interface
+
+#ifndef _HASH_H__
+#define _HASH_H__
+
+
+unsigned long hash1(char*);
+unsigned long hash2(char*);
+unsigned long hash3(char*);
+
+
+#endif
+
