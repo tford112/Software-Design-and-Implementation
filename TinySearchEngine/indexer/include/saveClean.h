5b7e9f5726b54eb5bd6d6685bdbd591d39da33db
Tom V <tomtomnv@gmail.com>
2020-07-14 01:44:23 -0400

    reorganized header files

---
+++ TinySearchEngine/indexer/include/saveClean.h
@@ -0,0 +1,12 @@
+#ifndef _SAVECLEAN_H_
+#define _SAVECLEAN_H_
+
+// *******************Implementation Spec *******************************
+//
+// File: saveClean.c 
+// This file contains the prototypes for saving a created index and cleaning up 
+// the memory afterwards 
+
+void saveIndex(INVERTED_INDEX*, char*, FILE*); 
+void cleanUp(INVERTED_INDEX*, FILE*); 
+#endif 
